5f3832a9cdbc9ab38612ec90c75ff3726cf53699
Sean Wilson <100713065+Leon-Swains@users.noreply.github.com>
2026-06-27 16:15:34 -0400

    Add QEMU

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,12 +12,9 @@ set -ouex pipefail
 # this installs a package from fedora repos
 # dnf5 install -y tmux 
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+# Install custom packages
+dnf5 -y install qemu-kvm
+dnf5 -y install libvirt
 
 #### Example for enabling a System Unit File
 
